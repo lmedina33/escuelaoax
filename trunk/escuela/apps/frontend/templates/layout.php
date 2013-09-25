1691bbf0e8a993c5a9c2beb90121e3afc0d490e7
jojegacu <jojegacu@dbcd53c1-49f2-4003-7e3e-ad3b4927926a>
2013-09-25 00:41:48 +0000

    ok

--- trunk/escuela/apps/frontend/templates/layout.php
+++ trunk/escuela/apps/frontend/templates/layout.php
@@ -9,51 +9,6 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <!--Menu-->
-    <div id="encabezadoSistema">
-        <div id="logoSistema"></div>
-        <div id="tituloSistema"><h1>Colegio Torres Bodet Preparatoria</h1></div>
-        <?php if($sf_user->getAttribute('admin')): ?>
-        <div id='cssmenu'>
-            <ul>
-                <li class='active'><a href=<?php echo url_for('inicio/index')?>><span>Inicio</span></a></li>
-                <li class='has-sub'><a href='#'><span>Catálogos</span></a>
-                    <ul>
-                        <li><a href=<?php echo url_for('profesor/index')?>><span>Profesores</span></a></li>
-                        <li><a href=<?php echo url_for('materia/index') ?>><span>Materias</span></a></li>
-                        <li><a href=<?php echo url_for('alumno/index')?>><span>Alumnos</span></a></li>
-                        <li><a href=<?php echo url_for('grupo/index')?>><span>Grupos</span></a></li>  
-                        <li class='last'><a href=<?php echo url_for('cuentas/index')?>><span>Cuentas de usuario</span></a></li>
-                    </ul>
-                </li>
-                <li class='has-sub'><a href='#'><span>Cursos</span></a>
-                    <ul>
-                        <li><a href=<?php echo url_for('periodoCalif/new')?>><span>Abrir periodo de calificaciones</span></a></li>
-                        <li><a href='#'><span>Editar calificaciones</span></a></li>
-                        <li><a href=<?php echo url_for('profesorMat/index')?>><span>Consultar cursos</span></a></li>
-                        <li class='last'><a href='#'><span>Consultar registros diarios</span></a></li>
-                    </ul>
-                </li>
-                <li class='has-sub'><a href='#'><span>Reportes</span></a>
-                    <ul>
-                        <li class='last'><a href='#'><span>Por Materia</span></a></li>
-                    </ul>
-                </li>
-                <li class="last"><a href=<?php echo url_for('logueo/logout')?>><span>Salir</span></a></li>
-            </ul>
-        </div>
-        <?php elseif($sf_user->getAttribute('profesor')):?>
-        <div id='cssmenu'>
-            <ul>
-                <li class='active'><a href=<?php echo url_for('inicio/index')?>><span>Inicio</span></a></li>
-                <li class='has-sub'><a href=<?php echo url_for('registroDiario/index')?>><span>Registro Diario</span></a></li>
-                <li class='has-sub'><a href='<?php echo url_for('registroDiario/subirCalificaciones')?>'><span>Subir Calificaciones</span></a></li>
-                <li class="last"><a href=<?php echo url_for('logueo/logout')?>><span>Salir</span></a></li>
-            </ul>
-        </div>
-        <?php endif;?>
-    </div>
-      
     <?php echo $sf_content ?>
   </body>
 </html>
